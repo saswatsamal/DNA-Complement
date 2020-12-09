94918a1fd14af5f7e9876d468518a757fa0cf5fe
Saswat Samal <39031660+saswatsamal@users.noreply.github.com>
2020-12-09 08:34:58 +0530

    Adding other bases
    Previously I've added the ATGC, now it has: A T U G C Y R S W K M B D H V N

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 def complement_base(base):
         
     if base in 'Aa':
@@ -10,46 +12,53 @@ def complement_base(base):
         return 'C'
     elif base in 'Cc':
         return 'G'
+    elif base in 'Yy':
+        return 'R'
+    elif base in 'Rr':
+        return 'Y'
+    elif base in 'Ss':
+        return 'S*'
+    elif base in 'Ww':
+        return 'W*'
+    elif base in 'Kk':
+        return 'M'
+    elif base in 'Mm':
+        return 'K'
+    elif base in 'Bb':
+        return 'V'
+    elif base in 'Vv':
+        return 'B'
+    elif base in 'Dd':
+        return 'H'
+    elif base in 'Hh':
+        return 'D'
     else:
         return 'N'
-
-
+        
 def reverse_complement(seq):
-    
-    
     
     rev_seq = ''
     
-    
     for base in reversed(seq):
         rev_seq += complement_base(base)
-        
-    print("Reverse: ",rev_seq)
+
+    print("Reverse:",rev_seq)
     return rev_seq
 
 def display_complements(seq):
-    
-        
-    
+      
     rev_comp = reverse_complement(seq)
     
-    
     print(seq)
-    
     
     for base in seq:
         print('|', end='')
     
-    
     print()
             
-    
     for base in reversed(rev_comp):
         print(base, end='')
         
     
-
 seq = input("Enter: ")
 display_complements(seq)
-
-    
